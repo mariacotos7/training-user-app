480fefbac7c4a0da02b5ad2fe82d459473321886
NicoViregan <nico_viregan@yahoo.com>
2020-05-01 13:25:12 +0300

    added auth files

---
+++ training_api/lib/models/user.ex
@@ -0,0 +1,19 @@
+defmodule Training.User do
+   #defstruct username: "costel", password: "costel", id: 1, role: 1, first_name: "cost", last_name: "el", experience: "3 years"
+   @db_name Application.get_env(:training, :redb_db)
+   @db_table "users"
+
+   use Training.Models.Base
+
+   defstruct [
+     :id,
+     :username,
+     :password,
+     :role,
+     :fist_name,
+     :last_name,
+     :experience,
+     :created_at,
+     :updated_at
+   ]
+ end

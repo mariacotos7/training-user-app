b70c194441134134168df87ccc5e8aa16b4c67b9
Maria Cotos <maria.cotos7@yahoo.com>
2020-05-24 15:41:56 +0300

    changed elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Training.MixProject do
     [
       app: :training,
       version: "0.1.0",
-      elixir: "~> 1.5.3",
+      elixir: "~> 1.1.1",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]

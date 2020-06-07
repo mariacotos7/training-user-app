d835df15fac45e8604effa754ea3034501cc3dd6
Maria Cotos <maria.cotos7@yahoo.com>
2020-06-07 13:36:13 +0300

    configure db for deploy

--- config/config.exs
+++ config/config.exs
@@ -7,9 +7,9 @@ use Mix.Config
 config :joken, default_signer: "secret2"
 
 config :training,
-  redb_host: "localhost",
-  redb_port: 28015,
-  redb_db: "test",
+  redb_host: "dpg-br58f2pd2beie4pd1g2g",
+  redb_port: 5432,
+  redb_db: "test_gin4",
   redb_tables: [
     "users"
   ],
